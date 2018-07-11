c69db522680baceba1c10fa598eced3598b573ab
Mark Borcherding <markborcherding@gmail.com>
2018-07-10 20:29:35 -0500

    add raw loader

--- 2018-08-03-pointless-javascript/pointless-javascript/webpack.config.js
+++ 2018-08-03-pointless-javascript/pointless-javascript/webpack.config.js
@@ -21,34 +21,45 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.md$/,
-      loader: "html-loader!markdown-loader?gfm=false"
-    }, {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: "babel-loader",
-      include: __dirname
-    }, {
-      test: /\.css$/,
-      loaders: ["style-loader", "raw-loader"],
-      include: __dirname
-    }, {
-      test: /\.svg$/,
-      loader: "url-loader?limit=10000&mimetype=image/svg+xml",
-      include: path.join(__dirname, "assets")
-    }, {
-      test: /\.png$/,
-      loader: "url-loader?mimetype=image/png",
-      include: path.join(__dirname, "assets")
-    }, {
-      test: /\.gif$/,
-      loader: "url-loader?mimetype=image/gif",
-      include: path.join(__dirname, "assets")
-    }, {
-      test: /\.jpg$/,
-      loader: "url-loader?mimetype=image/jpg",
-      include: path.join(__dirname, "assets")
-    }]
+    loaders: [
+      {
+        test: /\.txt$/,
+        loader: "raw-loader"
+      },
+      {
+        test: /\.md$/,
+        loader: "html-loader!markdown-loader?gfm=false"
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        include: __dirname
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "raw-loader"],
+        include: __dirname
+      },
+      {
+        test: /\.svg$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+        include: path.join(__dirname, "assets")
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?mimetype=image/png",
+        include: path.join(__dirname, "assets")
+      },
+      {
+        test: /\.gif$/,
+        loader: "url-loader?mimetype=image/gif",
+        include: path.join(__dirname, "assets")
+      },
+      {
+        test: /\.jpg$/,
+        loader: "url-loader?mimetype=image/jpg",
+        include: path.join(__dirname, "assets")
+      }]
   }
 };
